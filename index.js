fdac776ca044e3f83509a6821d34b4b02db517e2
‘peter-robert-cole’ <petercole3@gmail.com>
2020-02-11 07:54:17 -0800

    closer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const Input = require('./lib/input.js')
-const notes = require('./lib/notes.js')
+const Input = require('./lib/input.js');
+const notes = require('./lib/notes.js');
 
 const options = new Input();
 
